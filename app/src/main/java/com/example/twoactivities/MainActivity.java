f96e7169a6e9c8aa9ad26992c20b5db1a856b52c
NicoLoreto <nicoloreto.dev@gmail.com>
2023-10-19 21:17:08 -0300

    two activities created, sending data from one to the other, performed

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -6,8 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    // constant publica en la parte superior de la clase para definir la llave
+    // para el Intent extra
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.twoactivities.extra.MESSAGE";
+
+    // variable privada en la parte superior de la clase para contener el EditText
+    private EditText mMessageEditText;
 
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
@@ -16,13 +25,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  obtener la referencia al EditText y asignarlo
+        // a la variable privada
+        mMessageEditText = findViewById(R.id.editText_main);
+
+
     }
 
     public void launchSecondActivity(View view) {
 
+        // intent
         Log.d(LOG_TAG, "¡Botón presionado!");
         Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+        // obtenga el texto de
+        // EditText como una cadena
+        String message = mMessageEditText.getText().toString();
 
+        // Agregue esa cadena al Intent como un extra con la constante EXTRA_MESSAGE como
+        // clave y la cadena como valor.
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -2,7 +2,9 @@ package com.example.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        // Obtenga la Intent que activó esta actividad:
+        Intent intent = getIntent();
+
+        //  Obtenga la cadena que contiene el mensaje de los extras de Intent utilizando la variable
+        // estática MainActivity.EXTRA_MESSAGE como clave
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Use findViewByID() para obtener una referencia aTextView para el mensaje del diseño.
+        TextView textView = findViewById(R.id.text_message);
+
+        // Establezca el texto de TextView en la cadena del Intent extra:
+        textView.setText(message);
     }
 }
